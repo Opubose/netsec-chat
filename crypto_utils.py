58ad54a2c7764a98c40ddc51a2eac632c0c47fc0
Opubose <agastyabose@hotmail.com>
2025-12-07 02:22:03 -0600

    actually kinda secure parameters this time

--- crypto_utils.py
+++ crypto_utils.py
@@ -1,6 +1,7 @@
 import hmac
 import hashlib
 import base64
+import secrets
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
@@ -10,10 +11,25 @@ from typing import Tuple, Union
 HASH_FUNC = hashlib.sha256
 
 """
-diffie-hellman params. these are intentionally small for demonstration purposes only. pls don't actually use these in real life LOL.
+diffie-hellman params taken from RFC 3526 group 14 (2048-bit)
+> https://datatracker.ietf.org/doc/html/rfc3526#section-3
 """
-p = 23
-g = 5
+p_hex = (
+    "FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1"
+    "29024E088A67CC74020BBEA63B139B22514A08798E3404DD"
+    "EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245"
+    "E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED"
+    "EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D"
+    "C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F"
+    "83655D23DCA3AD961C62F356208552BB9ED529077096966D"
+    "670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B"
+    "E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9"
+    "DE2BCBF6955817183995497CEA956AE515D2261898FA0510"
+    "15728E5A8AACAA68FFFFFFFFFFFFFFFF"
+)
+p = int(p_hex, 16)
+
+g = 2
 
 
 def load_rsa_private_key(filepath: str) -> RSAPrivateKey:
@@ -131,9 +147,7 @@ def keyed_hash_decrypt(
 
 def generate_dh_private_key() -> int:
     """Generate a private key for Diffie-Hellman key exchange"""
-    import random
-
-    return random.randint(5, 15)
+    return secrets.randbits(256)
 
 
 def compute_dh_public_key(private_key: int) -> int:
