930bc759bdae1a10971f53134b49eb78662a5e2d
Opubose <agastyabose@hotmail.com>
2025-12-07 02:21:11 -0600

    improved window pane creation and implemented scuffed concurrency

--- run-chat.sh
+++ run-chat.sh
@@ -1,10 +1,20 @@
-# !/bin/bash
+#!/bin/bash
+
+tmux kill-session -t chat 2>/dev/null
+rm -f .chat.lock
 
 if [[ ! -e ./relay_private_key.pem ]]; then
     python3 keygen.py
 fi
 
-tmux new -d -s chat "python3 relay.py"   
-tmux split-window -h -t chat.0 "python3 client.py bob" 
-tmux split-window -h -t chat.0 "python3 client.py alice" 
-tmux attach-session -t chat
+tmux new-session -d -s netsec-chat -n session "python3 relay.py"
+
+tmux move-window -t netsec-chat:1 2>/dev/null || true
+
+tmux split-window -v -t netsec-chat:session "sleep 2; python3 client.py"
+
+tmux split-window -h -t netsec-chat:session "sleep 1; python3 client.py"
+
+tmux select-pane -t netsec-chat:session -L
+
+tmux attach-session -t netsec-chat
